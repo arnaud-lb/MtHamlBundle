0b97c3b0bb075c868c98c91e4bcb5d744bb4a3cb
Arnaud Le Blanc <arnaud.lb@gmail.com>
2011-11-27 13:26:45 +0100

    proxy TwigEngine instead of extending
    fixes latest symfony

--- MtHamlEngine.php
+++ MtHamlEngine.php
@@ -12,8 +12,27 @@ use Symfony\Bundle\TwigBundle\TwigEngine;
 /**
  * This engine knows how to render MtHaml templates.
  */
-class MtHamlEngine extends TwigEngine
+class MtHamlEngine implements EngineInterface
 {
+    private $twigEngine;
+    private $parser;
+
+    public function __construct(TwigEngine $twigEngine, TemplateNameParserInterface $parser)
+    {
+        $this->twigEngine = $twigEngine;
+        $this->parser = $parser;
+    }
+
+    public function render($name, array $parameters = array())
+    {
+        return $this->twigEngine->render($name, $parameters);
+    }
+
+    public function exists($name)
+    {
+        return $this->twigEngine->exists($name);
+    }
+
     /**
      * Returns true if this class is able to render the given template.
      *
@@ -26,6 +45,16 @@ class MtHamlEngine extends TwigEngine
         $template = $this->parser->parse($name);
 
         return 'haml' === $template->get('engine');
+    }
+
+    public function renderResponse($view, array $parameters = array(), Response $response = null)
+    {
+        return $this->twigEngine->renderResponse($view, $parameters, $response);
+    }
+
+    public function load($name)
+    {
+        return $this->twigEngine->load($name);
     }
 }
 
