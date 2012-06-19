05197e69684b8ab3165dcc5a17c0f2ed40b7dc98
Black Akula <blackakula@gmail.com>
2012-06-20 00:32:03 +0300

    Added Haml annotation instead of Template one

---
+++ Controller/Annotations/Haml.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MtHamlBundle\Controller\Annotations;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * @Annotation
+ */
+class Haml extends Template
+{
+
+    /**
+     * The haml engine used by default
+     *
+     * @var string
+     */
+    protected $engine = 'haml';
+}
---
+++ Tests/Fixtures/Controller/AnnotatedHamlController.php
@@ -0,0 +1,13 @@
+<?php
+namespace MtHamlBundle\Tests\Fixtures\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use MtHamlBundle\Controller\Annotations\Haml;
+
+class AnnotatedHamlController extends Controller
+{
+    /**
+     * @Haml()
+     */
+    public function getSomethingAction()
+    {}
+}
