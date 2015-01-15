0d02408d6f410b4d011948a71e59c19413034715
Ion Marusic <ion.marusic@gmail.com>
2015-01-15 16:49:57 +0200

    Fixed the HamlFileExtractor to compile the haml code to twig before extraction.

--- Translation/Extractor/File/HamlFileExtractor.php
+++ Translation/Extractor/File/HamlFileExtractor.php
@@ -7,11 +7,24 @@ use JMS\TranslationBundle\Model\MessageCatalogue;
 
 class HamlFileExtractor extends TwigFileExtractor
 {
+    /**
+     * @var \Twig_Environment
+     */
     private $twig;
 
     public function setTwig(\Twig_Environment $twig)
     {
         $this->twig = $twig;
+    }
+
+    /**
+     * @var \MtHaml\Environment
+     */
+    private $mtHaml;
+
+    public function setMtHaml(\MtHaml\Environment $mtHaml)
+    {
+        $this->mtHaml = $mtHaml;
     }
 
     public function visitFile(\SplFileInfo $file, MessageCatalogue $catalogue)
@@ -21,7 +34,9 @@ class HamlFileExtractor extends TwigFileExtractor
 
             if (in_array($extension, array('haml'))) {
                 $args = func_get_args();
-                $args[] = $this->twig->parse($this->twig->tokenize(file_get_contents($file), (string) $file));
+                $code = file_get_contents($file);
+                $code = $this->mtHaml->compileString($code, (string) $file);
+                $args[] = $this->twig->parse($this->twig->tokenize($code, (string) $file));
                 call_user_func_array(array($this, 'visitTwigFile'), $args);
             }
         }
